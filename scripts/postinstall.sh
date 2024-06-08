cf0591c9a967c0258de737de561fb8339652bcfd
Connor van Spronssen <info@dobefu.com>
2024-06-08 12:33:35 +0200

    Fix an issue where the postinstall script would fail randomly

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -11,9 +11,9 @@ cd "$SCRIPT_DIR/.." || exit 1
 # If the .env file doesn't exist, copy the example one.
 if [ ! -f ".env" ]; then
   # Generate a random secret for Auth.js.
-  AUTH_SECRET="$(openssl rand -base64 33)"
+  AUTH_SECRET="$(openssl rand -base64 33 | sed 's/[\\&*./+!]/\\&/g')"
 
   # Create a .env file from the .env.example file.
-  ENV="$(sed "s/^AUTH_SECRET=/AUTH_SECRET=\"\\$AUTH_SECRET\"/" .env.example)"
+  ENV="$(sed "s|^AUTH_SECRET=|AUTH_SECRET=\"$AUTH_SECRET\"|" .env.example)"
   echo "$ENV" > .env
 fi
