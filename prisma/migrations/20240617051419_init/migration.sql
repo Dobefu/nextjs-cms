2b8dbc0dedcb674379a76842b85b351daa1f6ccc
Connor van Spronssen <connor.van.spronssen@iodigital.com>
2024-06-17 07:18:24 +0200

    Remove the unique constraint from content titles

--- prisma/migrations/20240617051419_init/migration.sql
+++ prisma/migrations/20240617051419_init/migration.sql
@@ -41,7 +41,6 @@ CREATE TABLE `Content` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `Content_title_key`(`title`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
